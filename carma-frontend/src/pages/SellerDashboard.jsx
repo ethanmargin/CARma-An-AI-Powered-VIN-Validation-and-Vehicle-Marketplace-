808a5964b83ded2fb322b7a7a810b4375c964928
ethanmargin <ewpmargin@mymail.mapua.edu.ph>
2025-10-21 23:23:44 +0800

    remove logoutu

--- carma-frontend/src/pages/SellerDashboard.jsx
+++ carma-frontend/src/pages/SellerDashboard.jsx
@@ -72,12 +72,7 @@ function SellerDashboard() {
                 <span>Profile</span>
               </button>
               
-              <button
-                onClick={handleLogout}
-                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition"
-              >
-                Logout
-              </button>
+             
             </div>
           </div>
         </div>
